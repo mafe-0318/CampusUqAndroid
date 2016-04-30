0ea1138e30ed8357baadb426ded42ada7fb872b8
mafe lopez <mafe-0318@hotmail.com>
2016-04-30 10:47:56 -0500

    Se realizo el menu que se va a implementar en todas las interfaces

--- app/src/main/java/proyecto/android/ingesis/uniquindio/edu/co/campusuq/MainActivity.java
+++ app/src/main/java/proyecto/android/ingesis/uniquindio/edu/co/campusuq/MainActivity.java
@@ -2,6 +2,8 @@ package proyecto.android.ingesis.uniquindio.edu.co.campusuq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.Lenguaje)
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
